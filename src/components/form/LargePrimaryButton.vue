a5d3a38566a57ecdb11a3271fb58a5a210750ba2
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2023-05-10 17:12:36 +0200

    fixed button svg color

--- src/components/form/LargePrimaryButton.vue
+++ src/components/form/LargePrimaryButton.vue
@@ -1,5 +1,17 @@
 <template>
-  <button class="flex gap-2 justify-center items-center h-14 bg-gradient-to-b from-indigo-500 to-indigo-600 text-gray-100 font-bold uppercase p-4 w-full rounded-full">
+  <button class="fancy-button">
     <slot />
   </button>
 </template>
+
+<style scoped>
+button.fancy-button {
+  @apply flex gap-2 justify-center items-center h-14 bg-gradient-to-b from-indigo-500 to-indigo-600 text-gray-100 font-bold uppercase p-4 w-full rounded-full;
+}
+</style>
+
+<style>
+button.fancy-button svg {
+  @apply fill-gray-100;
+}
+</style>
